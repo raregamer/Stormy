747fd65f4a4b0ce7ae9fac6481df5ed26b93fcd3
Samuel <raregamer@hotmail.com>
2018-04-15 14:29:13 -0700

    Initial commit

---
+++ app/src/main/java/com/example/samuel/stormy/MainActivity.java
@@ -0,0 +1,154 @@
+package com.example.samuel.stormy;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+    //Current Weather variable.
+    private CurrentWeather mCurrentWeather;
+    @BindView(R.id.time_label) TextView mTimeLabel;
+    @BindView(R.id.temperature_label) TextView mTemperatureLabel;
+    @BindView(R.id.humidity_value) TextView mHumidityValue;
+    @BindView(R.id.precipitation_value) TextView mPrecipitationValue;
+    @BindView(R.id.summary_label) TextView mSummaryLabel;
+    @BindView(R.id.icon_imageview) ImageView mIconImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        String apiKey = "c3892d2c58404ce893d2ddd4f3ef1988";
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+
+        String forecastURL = "https://api.darksky.net/forecast/" +
+                apiKey +
+                "/" +
+                latitude +
+                "," +
+                longitude;
+
+        if(isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(forecastURL)
+                    .build();
+
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+
+                    try {
+                        String jsonData = response.body().string();
+                        Log.v(TAG,jsonData);
+
+                        if (response.isSuccessful()) {
+                            mCurrentWeather = getCurrentDetails(jsonData);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    updateDisplay();
+
+                                }
+                            });
+
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    } catch (JSONException e){
+                        Log.e(TAG,"Exception Caught: ", e);
+                    }
+
+                }
+            });
+        } else {
+            Toast.makeText(this, R.string.network_toast_unavailable, Toast.LENGTH_LONG).show();
+            alertUserAboutError();
+
+
+        }
+
+        Log.d(TAG, "Main UI Code is running!");
+
+
+    }
+
+    private void updateDisplay() {
+        mTemperatureLabel.setText(mCurrentWeather.getTemperature() + "");
+        mTimeLabel.setText("At " + mCurrentWeather.getFormattedTime() + " it will be");
+        mHumidityValue.setText(mCurrentWeather.getHumidity() + "");
+        mPrecipitationValue.setText(mCurrentWeather.getPrecipitationChance() + "%");
+        mSummaryLabel.setText(mCurrentWeather.getSummary());
+
+    }
+
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
+
+        JSONObject forecast = new JSONObject(jsonData);
+        String timeZone = forecast.getString("timezone");
+        JSONObject currently = forecast.getJSONObject("currently");
+
+        CurrentWeather currentWeather = new CurrentWeather();
+        currentWeather.setHumidity(currently.getDouble("humidity"));
+        currentWeather.setPrecipitationChance(currently.getDouble("precipProbability"));
+        currentWeather.setTemperature(currently.getDouble("temperature"));
+        currentWeather.setTime(currently.getLong("time"));
+        currentWeather.setIcon(currently.getString("icon"));
+        currentWeather.setSummary(currently.getString("summary"));
+        currentWeather.setTimeZone(timeZone);
+
+//        Log.d(TAG, currentWeather.getFormattedTime());
+//
+//        Log.i(TAG, "From JSON: " + timeZone + " " + currentWeather.getSummary() + " " + currentWeather.getIcon() + " " +
+//        currentWeather.getHumidity() + " " + currentWeather.getTime() + " " + currentWeather.getTemperature() + " " + currentWeather.getPrecipitationChance());
+
+        return currentWeather;
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if(networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(),"error_dialog");
+    }
+}
